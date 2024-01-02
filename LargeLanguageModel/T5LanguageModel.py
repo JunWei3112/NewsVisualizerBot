1dde863bc4e995abe6b3ee42579a64ebe5d8191a
JunWei3112 <e0543453@u.nus.edu>
2024-01-02 22:44:37 +0800

    Refactor code in T5LanguageModel

--- LargeLanguageModel/T5LanguageModel.py
+++ LargeLanguageModel/T5LanguageModel.py
@@ -35,8 +35,7 @@ def run_diagnostics(llm_generate_text):
     print(f'Number of correct annotations: {correct_annotations}/{number_of_annotations}')
     print('------------------------------')
 
-if __name__ == '__main__':
-    model_path = 'google/flan-t5-large-instruction-type-tuned'
+def generate_pipeline(model_path):
     device = f'cuda:{torch.cuda.current_device()}' if torch.cuda.is_available() else 'cpu'
     print(f"Using device: {device}")
 
@@ -58,83 +57,104 @@ if __name__ == '__main__':
             max_new_tokens=256,
         )
 
+    return generate_text
+
+def identify_instruction_type(pipeline, instruction):
+    prompt_instruction_type = f'For the following instruction to modify an infographic ({instruction}), what is the instruction type (ADD/DELETE/EDIT/MOVE)?'
+
+    instruction_type_obj = pipeline(prompt_instruction_type)
+    instruction_type = instruction_type_obj[0]['generated_text']
+    return instruction_type
+
+def identify_target_element(pipeline, instruction, instruction_type):
+    prompt_target_element = f'For the following instruction to modify an infographic ({instruction}), what is the element that is to be '
+    if instruction_type == 'ADD':
+        prompt_target_element += 'added?'
+    elif instruction_type == 'DELETE':
+        prompt_target_element += 'deleted?'
+    elif instruction_type == 'EDIT':
+        prompt_target_element += 'modified?'
+    else:
+        prompt_target_element += 'moved?'
+
+    target_element_obj = pipeline(prompt_target_element)
+    target_element = target_element_obj[0]['generated_text']
+    return target_element
+
+def identify_infographic_section(pipeline, instruction, instruction_type):
+    if instruction_type == 'ADD':
+        prompt_infographic_section = f'For the following instruction to modify an infographic ({instruction}), what is the infographic section (1) Number of Shares, 2) Vote on Reliability, 3) Related Facts, 4) Latest Comments, 5) Knowledge Graph Summaries, 6) Similar Articles, 7) Header) where the target element will be added to? '
+    elif instruction_type == 'DELETE':
+        prompt_infographic_section = f'For the following instruction to modify an infographic ({instruction}), what is the infographic section (1) Number of Shares, 2) Vote on Reliability, 3) Related Facts, 4) Latest Comments, 5) Knowledge Graph Summaries, 6) Similar Articles, 7) Header) where the target element will be deleted from? '
+    elif instruction_type == 'EDIT':
+        prompt_infographic_section = f'For the following instruction to modify an infographic ({instruction}), what is the infographic section (1) Number of Shares, 2) Vote on Reliability, 3) Related Facts, 4) Latest Comments, 5) Knowledge Graph Summaries, 6) Similar Articles, 7) Header) where the modified element is in? '
+    else:
+        prompt_infographic_section = f'For the following instruction to modify an infographic ({instruction}), what is the infographic section (1) Number of Shares, 2) Vote on Reliability, 3) Related Facts, 4) Latest Comments, 5) Knowledge Graph Summaries, 6) Similar Articles, 7) Header) where the target element is originally in?  '
+    prompt_infographic_section += 'If unable to infer the infographic section, set the answer as NONE.'
+
+    infographic_section_obj = pipeline(prompt_infographic_section)
+    infographic_section = infographic_section_obj[0]['generated_text']
+    return infographic_section
+
+def identify_target_location(pipeline, instruction, instruction_type):
+    if instruction_type == 'ADD':
+        prompt_target_location = f'For the following instruction to modify an infographic, ({instruction}), what is the target location of the new element in the infographic section? If unable to infer the location, set the answer as NONE.'
+    elif instruction_type == 'MOVE':
+        prompt_target_location = f'For the following instruction to modify an infographic, ({instruction}), what is the new location of the target element?'
+
+    target_location_obj = pipeline(prompt_target_location)
+    target_location = target_location_obj[0]['generated_text']
+    return target_location
+
+def identify_edit_attribute(pipeline, instruction):
+    prompt_edit_attribute = f'For the following instruction to modify an infographic, ({instruction}), if the size of the target element is modified, set the answer as SIZE. Else, if the textual content of the target element is modified, set the answer as CONTENT.'
+
+    edit_attribute_obj = pipeline(prompt_edit_attribute)
+    edit_attribute = edit_attribute_obj[0]['generated_text']
+    return edit_attribute
+
+def identify_edit_value(pipeline, instruction):
+    prompt_edit_value = f'For the following instruction to modify an infographic, ({instruction}), what is the new size or textual content of the target element?'
+
+    edit_value_obj = pipeline(prompt_edit_value)
+    edit_value = edit_value_obj[0]['generated_text']
+    return edit_value
+
+def generate_structured_output(pipeline, instruction):
+    with CodeTimer('Generating Structured Output', unit='s'):
+        # Identify Instruction Type
+        instruction_type = identify_instruction_type(pipeline, instruction)
+        print(f'Instruction Type: {instruction_type}')
+
+        # Identify Target Element
+        target_element = identify_target_element(pipeline, instruction, instruction_type)
+        print(f'Target Element: {target_element}')
+
+        # Identify Infographic Section
+        infographic_section = identify_infographic_section(pipeline, instruction, instruction_type)
+        print(f'Infographic Section: {infographic_section}')
+
+        # Identify Target Location of Element
+        if instruction_type == 'ADD' or instruction_type == 'MOVE':
+            target_location = identify_target_location(pipeline, instruction, instruction_type)
+            print(f'Target Location: {target_location}')
+
+        # Identify Edit Attribute and Target Value
+        if instruction_type == 'EDIT':
+            edit_attribute = identify_edit_attribute(pipeline, instruction)
+            print(f'Edit Attribute: {edit_attribute}')
+
+            edit_value = identify_edit_value(pipeline, instruction)
+            print(f'Edit Value: {edit_value}')
+
+if __name__ == '__main__':
+    model_path = 'google/flan-t5-large-instruction-type-tuned'
+    generate_text = generate_pipeline(model_path)
+
     instruction = "add a comment: 'This is a good article'"
     print('-------------------------------------')
     print(f'Instruction: {instruction}')
     print('-------------------------------------')
 
-    with CodeTimer('Generating Structured Output', unit='s'):
-        # Identify Instruction Type
-        prompt_instruction_type = f'For the following instruction to modify an infographic ({instruction}), what is the instruction type (ADD/DELETE/EDIT/MOVE)?'
-
-        instruction_type_obj = generate_text(prompt_instruction_type)
-        instruction_type = instruction_type_obj[0]['generated_text']
-        print(f'Instruction Type: {instruction_type}')
-
-        # Identify Target Element
-        prompt_target_element = f'For the following instruction to modify an infographic ({instruction}), what is the element that is to be '
-        if instruction_type == 'ADD':
-            prompt_target_element += 'added?'
-        elif instruction_type == 'DELETE':
-            prompt_target_element += 'deleted?'
-        elif instruction_type == 'EDIT':
-            prompt_target_element += 'modified?'
-        else:
-            prompt_target_element += 'moved?'
-
-        target_element_obj = generate_text(prompt_target_element)
-        target_element = target_element_obj[0]['generated_text']
-        print(f'Target Element: {target_element}')
-
-        # Identify Infographic Section
-        if instruction_type == 'ADD':
-            prompt_infographic_section = f'For the following instruction to modify an infographic ({instruction}), what is the infographic section (1) Number of Shares, 2) Vote on Reliability, 3) Related Facts, 4) Latest Comments, 5) Knowledge Graph Summaries, 6) Similar Articles, 7) Header) where the target element will be added to? '
-        elif instruction_type == 'DELETE':
-            prompt_infographic_section = f'For the following instruction to modify an infographic ({instruction}), what is the infographic section (1) Number of Shares, 2) Vote on Reliability, 3) Related Facts, 4) Latest Comments, 5) Knowledge Graph Summaries, 6) Similar Articles, 7) Header) where the target element will be deleted from? '
-        elif instruction_type == 'EDIT':
-            prompt_infographic_section = f'For the following instruction to modify an infographic ({instruction}), what is the infographic section (1) Number of Shares, 2) Vote on Reliability, 3) Related Facts, 4) Latest Comments, 5) Knowledge Graph Summaries, 6) Similar Articles, 7) Header) where the modified element is in? '
-        else:
-            prompt_infographic_section = f'For the following instruction to modify an infographic ({instruction}), what is the infographic section (1) Number of Shares, 2) Vote on Reliability, 3) Related Facts, 4) Latest Comments, 5) Knowledge Graph Summaries, 6) Similar Articles, 7) Header) where the target element is originally in?  '
-        prompt_infographic_section += 'If unable to infer the infographic section, set the answer as NONE.'
-
-        infographic_section_obj = generate_text(prompt_infographic_section)
-        infographic_section = infographic_section_obj[0]['generated_text']
-        print(f'Infographic Section: {infographic_section}')
-
-        # # Identify Element Location
-        # if instruction_type != 'MOVE':
-        #     prompt_element_location = f'For the following instruction to modify an infographic, ({instruction}), '
-        #     if instruction_type == 'ADD':
-        #         prompt_element_location += 'what is the target location of the new element in the infographic section? '
-        #     elif instruction_type == 'DELETE':
-        #         prompt_element_location += 'what is the location of the deleted element in the infographic section? '
-        #     elif instruction_type == 'EDIT':
-        #         prompt_element_location += 'what is the location of the modified element in the infographic section? '
-        #     prompt_element_location += 'If unable to infer the location, set the answer as NONE. '
-        #
-        #     element_location_obj = generate_text(prompt_element_location)
-        #     element_location = element_location_obj[0]['generated_text']
-        #     print(f'Location of Target Element in Infographic Section: {element_location}')
-
-        # Identify Edit Attribute and Target Value
-        if instruction_type == 'EDIT':
-            prompt_edit_attribute = f'For the following instruction to modify an infographic, ({instruction}), if the size of the target element is modified, set the answer as SIZE. Else, if the textual content of the target element is modified, set the answer as CONTENT.'
-
-            edit_attribute_obj = generate_text(prompt_edit_attribute)
-            edit_attribute = edit_attribute_obj[0]['generated_text']
-            print(f'Edit Attribute: {edit_attribute}')
-
-            prompt_target_value = f'For the following instruction to modify an infographic, ({instruction}), what is the new size or textual content of the target element?'
-
-            target_value_obj = generate_text(prompt_target_value)
-            target_value = target_value_obj[0]['generated_text']
-            print(f'Target Value: {target_value}')
-
-        # Identify New Location
-        if instruction_type == 'MOVE':
-            prompt_new_location = f'For the following instruction to modify an infographic, ({instruction}), what is the new location of the target element?'
-
-            new_location_obj = generate_text(prompt_new_location)
-            new_location = new_location_obj[0]['generated_text']
-            print(f'New Location: {new_location}')
+    generate_structured_output(generate_text, instruction)
 
