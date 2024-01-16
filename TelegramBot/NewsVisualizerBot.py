b81913cad1be75f93ab924cc1bd78cae02c67cc5
JunWei3112 <e0543453@u.nus.edu>
2024-01-17 01:23:57 +0800

    Implement the following features: Users are able to vote for the reliability of an article Keep track of the current share count of an infographic Users are able to view the vote + share counts of an infographic Check for the validity of the infographic link that a user has sent

--- TelegramBot/NewsVisualizerBot.py
+++ TelegramBot/NewsVisualizerBot.py
@@ -6,7 +6,10 @@ import ResponseHandler
 # Initialise Telegram bot
 bot = telebot.TeleBot(config.BOT_TOKEN, parse_mode=None)
 
-news_articles_cluster = None
+database = None
+
+# To keep track of the infographic links that the users have entered
+latest_infographic_links = {}
 
 def startup_telegram_bot():
     bot.polling(non_stop=True)
@@ -18,6 +21,8 @@ def start_command(message):
     keyboard.add(visualize_news_button)
     modify_infographic_button = telebot.types.InlineKeyboardButton(text="Modify Infographic", callback_data="modify_infographic")
     keyboard.add(modify_infographic_button)
+    view_infographic_stats_button = telebot.types.InlineKeyboardButton(text="View Infographic Stats", callback_data="view_infographic_stats")
+    keyboard.add(view_infographic_stats_button)
     greeting_message = 'Greetings! What will you like to do today?'
     bot.send_message(message.chat.id, greeting_message, reply_markup=keyboard)
 
@@ -29,32 +34,101 @@ def callback_inline(call):
             sent = bot.send_message(call.message.chat.id, reply_message)
             bot.register_next_step_handler(sent, receive_news_article)
         elif call.data == 'modify_infographic':
-            reply_message = 'Please send me the infographic that you wish to modify.'
+            reply_message = 'Please send me the link to the infographic that you wish to modify.'
             sent = bot.send_message(call.message.chat.id, reply_message)
             bot.register_next_step_handler(sent, receive_infographic)
+        elif call.data == 'view_infographic_stats':
+            reply_message = 'Please send me the link to the infographic that you wish to view the stats for.'
+            sent = bot.send_message(call.message.chat.id, reply_message)
+            bot.register_next_step_handler(sent, view_infographic_stats)
+        elif call.data == 'vote_for_reliability':
+            keyboard = telebot.types.InlineKeyboardMarkup()
+            vote_reliable_button = telebot.types.InlineKeyboardButton(text="Reliable",
+                                                                      callback_data="vote_reliable")
+            keyboard.add(vote_reliable_button)
+            vote_unreliable_button = telebot.types.InlineKeyboardButton(text="Unreliable",
+                                                                        callback_data="vote_unreliable")
+            keyboard.add(vote_unreliable_button)
+
+            reply_message = "Do you view this news article as reliable or unreliable?"
+            bot.send_message(call.message.chat.id, reply_message, reply_markup=keyboard)
+        elif call.data == 'custom_modification':
+            reply_message = "What change do you wish to make to this infographic?"
+            sent = bot.send_message(call.message.chat.id, reply_message)
+            bot.register_next_step_handler(sent, receive_infographic_changes)
+        elif call.data == 'vote_reliable':
+            link_to_infographic = latest_infographic_links.get(call.message.chat.id)
+            if link_to_infographic is None:
+                print('User has not entered an infographic link.')
+            CommonDbOperations.increase_reliable_vote_count(database, link_to_infographic)
+            reply_message = "You have voted this article as reliable!"
+            bot.send_message(call.message.chat.id, reply_message)
+        elif call.data == 'vote_unreliable':
+            link_to_infographic = latest_infographic_links.get(call.message.chat.id)
+            if link_to_infographic is None:
+                print('User has not entered an infographic link.')
+            CommonDbOperations.increase_unreliable_vote_count(database, link_to_infographic)
+            reply_message = "You have voted this article as unreliable!"
+            bot.send_message(call.message.chat.id, reply_message)
 
 def receive_news_article(message):
-    reply_message = "This is the article that you sent: {}".format(message.text)
+    link_to_infographic = CommonDbOperations.receive_news_article(database, message.chat.id, message.text)
+    reply_message = "This is the link to access the generated infographic: {}".format(link_to_infographic)
     bot.send_message(message.chat.id, reply_message)
-    CommonDbOperations.store_news_articles(news_articles_cluster, message.chat.id, message.text)
 
 def receive_infographic(message):
-    reply_message = "What change(s) do you wish to make to this infographic?"
-    sent = bot.send_message(message.chat.id, reply_message)
-    bot.register_next_step_handler(sent, receive_infographic_changes)
+    link_to_infographic = message.text
+    is_valid_link = CommonDbOperations.check_if_infographic_link_exists(database, link_to_infographic)
+    if is_valid_link:
+        latest_infographic_links[message.chat.id] = link_to_infographic
+
+        CommonDbOperations.increase_share_count(database, link_to_infographic)
+
+        keyboard = telebot.types.InlineKeyboardMarkup()
+        reliability_vote_button = telebot.types.InlineKeyboardButton(text="Vote For Reliability Of News Article",
+                                                                     callback_data="vote_for_reliability")
+        keyboard.add(reliability_vote_button)
+        custom_modification_button = telebot.types.InlineKeyboardButton(text="Type Out Modification",
+                                                                        callback_data="custom_modification")
+        keyboard.add(custom_modification_button)
+
+        reply_message = "How will you like to modify the infographic?"
+        bot.send_message(message.chat.id, reply_message, reply_markup=keyboard)
+    else:
+        reply_message = "You have sent an invalid link."
+        bot.send_message(message.chat.id, reply_message)
 
 def receive_infographic_changes(message):
+    user_infographic = latest_infographic_links.get(message.chat.id)
+    if user_infographic is None:
+        print('User has not entered an infographic link.')
     proposed_user_changes = message.text
     processing_message = 'Generating instructions... ' + '\U000023F3'
     bot.send_message(message.chat.id, processing_message)
     ResponseHandler.generate_intermediate_representation(proposed_user_changes)
-    # generated_instructions = QueryProcessorInterface.process_query(proposed_user_changes)
-    # bot.send_message(message.chat.id, generated_instructions)
     bot.send_message(message.chat.id, "Completed")
 
+def view_infographic_stats(message):
+    link_to_infographic = message.text
+    is_valid_link = CommonDbOperations.check_if_infographic_link_exists(database, link_to_infographic)
+    if is_valid_link:
+        CommonDbOperations.increase_share_count(database, link_to_infographic)
+        infographic_document = CommonDbOperations.get_infographic_document(database, link_to_infographic)
+        infographic_stats = format_infographic_stats(infographic_document)
+        bot.send_message(message.chat.id, infographic_stats)
+    else:
+        reply_message = "You have sent an invalid link."
+        bot.send_message(message.chat.id, reply_message)
+
+def format_infographic_stats(infographic_document):
+    overview = f"Share Count: {infographic_document['share_count']}\n"
+    overview += f"Vote Counts for Reliable: {infographic_document['reliable_vote_count']}\n"
+    overview += f"Vote Counts for Unreliable: {infographic_document['unreliable_vote_count']}\n"
+    return overview
+
 def startup_database():
-    global news_articles_cluster
-    news_articles_cluster = CommonDbOperations.startup_database()
+    global database
+    database = CommonDbOperations.startup_database()
 
 if __name__ == '__main__':
     startup_database()
